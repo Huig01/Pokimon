a65170dccd903601c312294ad580beb0a3e7cf20
Huig <153827936+Huig01@users.noreply.github.com>
2025-03-30 19:58:09 +0200

    Clean up

---
+++ poki/src/Pokimon.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Pokimon = () => {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    const newTodos = axios("https://jsonplaceholder.typecode.com/todos").then(
+      (json) => json.data
+    );
+
+    setTodos(newTodos);
+  }, []);
+};
+
+export default Pokimon;
