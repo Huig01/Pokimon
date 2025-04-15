8fe09095b0a220fb2b065303bc59ebbc7234b0c9
Huig <153827936+Huig01@users.noreply.github.com>
2025-04-15 11:22:25 +0200

    Read me veranderd

--- poki/src/components/PokimonList.jsx
+++ poki/src/components/PokimonList.jsx
@@ -1,18 +1,3 @@
-/* import React from "react";
-
-const PokimonList = ({ pokemon }) => {
-  return (
-    <div>
-      {pokemon.map((p) => (
-        <div key={p}>{p}</div>
-      ))}
-    </div>
-  );
-};
-
-export default PokimonList;
- */
-
 import React from "react";
 
 const PokimonList = ({ pokemon, onPokemonClick }) => {
